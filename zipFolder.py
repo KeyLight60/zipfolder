b66aea7a12e52a73d12db7ef492070245474ac0f
KeyLight60 <87406214+KeyLight60@users.noreply.github.com>
2024-05-14 02:45:45 +0900

    Update zipFolder.py
    change comment

--- zipFolder.py
+++ zipFolder.py
@@ -9,14 +9,14 @@ import configparser as config
 
 
 # function
-# ini情報取得
+# ini information acquisition
 def getInfo(section, value):
     config_ini = config.ConfigParser()
     config_ini.read('setting.ini', encoding='utf-8')
     data = config_ini[section][value]
     return data
 
-# 日時取得
+# Get date and time
 def getDateTime():
     dt_now = dt.datetime.now()
     get_dt = dt_now.strftime('%Y_%m_%d_%H_%M_%S')
@@ -27,16 +27,16 @@ def main():
     arg2_1 = 'FOLDER_PATH'
     arg2_2 = 'DESTINATION_PATH'
 
-    # 情報取得
+    # Information acquisition
     f_path = getInfo(arg1, arg2_1)
     destination_path = getInfo(arg1, arg2_2)
     date = getDateTime()
 
-    # 指定フォルダzip化
+    # Zip specified folder
     shutil.make_archive(date, 'zip', root_dir = f_path)
     print('zipped path :' + f_path)
 
-    # zipファイル移動
+    # Move zip file
     source_path = (os.getcwd() + '\\' + date + '.zip')
     shutil.move(source_path, destination_path)
     print('Moved the file to ' + destination_path)
